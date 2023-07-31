d12ea612a675d332c2105502db19ad27384cef29
aaronFortuno <aaron.fortuno@protonmail.com>
2023-07-31 19:45:56 +0200

    Javadoc, clean code and some refactors

--- app/src/main/java/net/estemon/codelabs111_recyclerrecipes/MainActivity.java
+++ app/src/main/java/net/estemon/codelabs111_recyclerrecipes/MainActivity.java
@@ -1,7 +1,6 @@
 package net.estemon.codelabs111_recyclerrecipes;
 
 import android.annotation.SuppressLint;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -17,7 +16,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AlertDialog;
@@ -37,7 +35,13 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
+/**
+ * MainActivity que muestra una lista de recetas en un RecyclerView.
+ * Permite agregar nuevas recetas y ver los detalles de las recetas existentes.
+ */
 public class MainActivity extends AppCompatActivity implements RecipeAdapter.OnRecipeClickListener {
+
+    // Listas para almacenar los datos de las recetas
     private final List<String> ids = new ArrayList<>();
     private final List<String> titles = new ArrayList<>();
     private final List<String> resumes = new ArrayList<>();
@@ -48,21 +52,31 @@ public class MainActivity extends AppCompatActivity implements RecipeAdapter.OnR
     private RecyclerView mRecyclerView;
     private RecipeAdapter mAdapter;
 
+    // URI de la foto capturada con la cámara
     private Uri photoUri;
+
+    // ActivityResultLauncher para capturar la imagen
     private ActivityResultLauncher<Uri> captureImageLauncher;
+
 
     public MainActivity() {
     }
 
+    /**
+     * Crea y configura la actividad, inicializa la interfaz de usuario y las listas de recetas.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Inicializa la lista de IDs de recetas desde los recursos
         ids.addAll(Arrays.asList(this.getResources().getStringArray(R.array.recipe_ids)));
 
+        // Llena las listas de recetas con los datos de los recursos
         populateLists(titles, resumes, photos, details);
 
+        // Configura el RecyclerView y el adaptador
         mRecyclerView = findViewById(R.id.recycler_view);
         mAdapter = new RecipeAdapter(titles, resumes, details, photos, this);
 
@@ -75,26 +89,39 @@ public class MainActivity extends AppCompatActivity implements RecipeAdapter.OnR
         View dialogView = inflater.inflate(R.layout.add_recipe_dialog, null);
         final ImageView previewRecipePhoto = dialogView.findViewById(R.id.preview_recipe_photo);
 
-        captureImageLauncher = registerForActivityResult(
-                new ActivityResultContracts.TakePicture(),
-                new ActivityResultCallback<Boolean>() {
-                    @Override
-                    public void onActivityResult(Boolean result) {
-                        if (result) {
-                            previewRecipePhoto.setImageURI(photoUri);
-                        } else {
-                            Toast.makeText(MainActivity.this, "Error al capturar la imagen", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                }
-        );
+        // Configura el lanzador para capturar imágenes de la cámara
+        setupImageCaptureLauncher(previewRecipePhoto);
 
-        // FAB para añadir nuevas recetas
+        // Configura el botón flotante para agregar nuevas recetas
         FloatingActionButton fabAddRecipe = findViewById(R.id.fab_add_recipe);
         fabAddRecipe.setOnClickListener(view -> showAddRecipeDialog());
     }
 
+    /**
+     * Configura el ActivityResultLauncher para capturar imágenes de la cámara.
+     * Establece el resultado de la captura de la imagen en el ImageView de vista previa.
+     */
+    private void setupImageCaptureLauncher(ImageView previewRecipePhoto) {
+        captureImageLauncher = registerForActivityResult(
+                new ActivityResultContracts.TakePicture(),
+                result -> {
+                    if (result) {
+                        previewRecipePhoto.setImageURI(photoUri);
+                    } else {
+                        Toast.makeText(MainActivity.this, "Error al capturar la imagen", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+    }
 
+    /**
+     * Llena las listas de recetas con los datos de los recursos.
+     *
+     * @param titles   Lista para almacenar los títulos de las recetas.
+     * @param resumes  Lista para almacenar los resúmenes de las recetas.
+     * @param photos   Lista para almacenar las URIs de las fotos de las recetas.
+     * @param details  Lista para almacenar los detalles de las recetas.
+     */
     private void populateLists(List<String> titles, List<String> resumes, List<Uri> photos, List<String> details) {
         for (String id : ids) {
             // TODO: Verificar si existe el título
@@ -124,6 +151,14 @@ public class MainActivity extends AppCompatActivity implements RecipeAdapter.OnR
         }
     }
 
+    /**
+     * Método llamado cuando se hace clic en una receta en la lista.
+     * Muestra un cuadro de diálogo que muestra los detalles de la receta, incluido el título, contenido y una foto.
+     *
+     * @param title   Título de la receta.
+     * @param photo   URI de la foto de la receta. Puede ser nulo si no hay foto disponible.
+     * @param content Contenido o detalles de la receta.
+     */
     @Override
     public void onRecipeClick(String title, Uri photo, String content) {
 
@@ -161,6 +196,10 @@ public class MainActivity extends AppCompatActivity implements RecipeAdapter.OnR
         closeButton.setOnClickListener(view -> alertDialog.dismiss());
     }
 
+    /**
+     * Muestra un cuadro de diálogo para agregar una nueva receta.
+     * Permite al usuario ingresar los detalles de la receta y capturar una foto con la cámara.
+     */
     private void showAddRecipeDialog() {
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
         LayoutInflater inflater = this.getLayoutInflater();
@@ -193,35 +232,34 @@ public class MainActivity extends AppCompatActivity implements RecipeAdapter.OnR
             }
         });
 
-        dialogBuilder.setPositiveButton("Añadir receta", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                String recipeId = addRecipeId.getText().toString().trim();
-                String recipeTitle = addRecipeTitle.getText().toString().trim();
-                String recipeResume = addRecipeResume.getText().toString().trim();
-                String recipeDetails = addRecipeDetails.getText().toString().trim();
+        /*
+          Define el comportamiento del botón de confirmar los datos del diálogo
+          En este caso, mostrar en el listado de recetas los datos de la nueva receta
+         */
+        dialogBuilder.setPositiveButton("Añadir receta", (dialogInterface, i) -> {
+            String recipeId = addRecipeId.getText().toString().trim();
+            String recipeTitle = addRecipeTitle.getText().toString().trim();
+            String recipeResume = addRecipeResume.getText().toString().trim();
+            String recipeDetails = addRecipeDetails.getText().toString().trim();
 
-                ids.add(recipeId);
-                titles.add(recipeTitle);
-                resumes.add(recipeResume);
-                // photos.add(Integer.valueOf(String.valueOf(photoUri)));
-                details.add(recipeDetails);
+            ids.add(recipeId);
+            titles.add(recipeTitle);
+            resumes.add(recipeResume);
+            details.add(recipeDetails);
 
-                if (photoUri != null) {
-                    // int photoResId = R.drawable.ic_add;
-                    photos.add(photoUri);
-                } else {
-                    photos.add(null);
-                }
-
-                // Notificar al adaptador que se ha agregado un nuevo elemento en la última posición
-                int newPos = titles.size() - 1;
-                mAdapter.notifyItemInserted(newPos);
-                mAdapter.notifyDataSetChanged();
-
-                //
-                mRecyclerView.smoothScrollToPosition(newPos);
+            if (photoUri != null) {
+                photos.add(photoUri);
+            } else {
+                photos.add(null);
             }
+
+            // Notificar al adaptador que se ha agregado un nuevo elemento en la última posición
+            int newPos = titles.size() - 1;
+            mAdapter.notifyItemInserted(newPos);
+            mAdapter.notifyDataSetChanged();
+
+            //
+            mRecyclerView.smoothScrollToPosition(newPos);
         });
 
         dialogBuilder.setNegativeButton("Cancelar", null);
@@ -230,6 +268,11 @@ public class MainActivity extends AppCompatActivity implements RecipeAdapter.OnR
         alertDialog.show();
     }
 
+    /**
+     * Crea un archivo de imagen temporal para guardar la foto capturada con la cámara.
+     *
+     * @return El objeto File que representa el archivo de imagen temporal.
+     */
     private File createImageFile() {
         try {
             String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
--- app/src/main/java/net/estemon/codelabs111_recyclerrecipes/RecipeAdapter.java
+++ app/src/main/java/net/estemon/codelabs111_recyclerrecipes/RecipeAdapter.java
@@ -16,6 +16,9 @@ import com.bumptech.glide.Glide;
 
 import java.util.List;
 
+/**
+ * Adapter class for displaying recipes in a RecyclerView.
+ */
 public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeHolder> {
 
     private final List<String> titles;
@@ -23,8 +26,18 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeHold
     private final List<String> details;
     private final List<Uri> photos;
 
+    // Interface to handle clicks in the recipe elements.
     private OnRecipeClickListener recipeClickListener;
 
+    /**
+     * Constructor for the RecipeAdapter.
+     *
+     * @param titles   The list of recipe titles.
+     * @param resumes  The list of recipe resumes.
+     * @param details  The list of recipe details.
+     * @param photos   The list of photo URIs associated with each recipe.
+     * @param listener The listener to handle recipe item clicks.
+     */
     public RecipeAdapter(List<String> titles, List<String> resumes, List<String> details, List<Uri> photos, OnRecipeClickListener listener) {
         this.titles = titles;
         this.resumes = resumes;
@@ -33,6 +46,13 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeHold
         recipeClickListener = listener;
     }
 
+    /**
+     * Called when the RecyclerView needs a new {@link RecipeHolder} to represent an item.
+     *
+     * @param parent   The ViewGroup into which the new View will be added after it is bound to an adapter position.
+     * @param viewType The view type of the new View.
+     * @return A new {@link RecipeHolder} that holds a View of the given view type.
+     */
     @NonNull
     @Override
     public RecipeAdapter.RecipeHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -40,50 +60,91 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeHold
         return new RecipeHolder(recipeView);
     }
 
+    /**
+     * Called by RecyclerView to display the data at the specified position.
+     *
+     * @param holder   The ViewHolder which should be updated to represent the contents of the item at the given position
+     * @param position The position of the item within the adapter's data set
+     */
     @Override
     public void onBindViewHolder(@NonNull RecipeAdapter.RecipeHolder holder, int position) {
+
+        // Retrieve the data for the current position
         String title = titles.get(holder.getAdapterPosition());
         String resume = resumes.get(holder.getAdapterPosition());
         String detail = details.get(holder.getAdapterPosition());
         Uri photoUri = photos.get(holder.getAdapterPosition());
 
+        // Set the title and resume in the corresponding views
         holder.recipeTitleView.setText(title);
         holder.recipeResumeView.setText(resume);
 
+        // Load the image using Glide, if available
         if (photoUri != null) {
-            // photoUri = Uri.parse(String.valueOf(photoResId));
             Log.d("PopulateLists Glide", "photoUri: " + photoUri);
             Glide.with(holder.itemView.getContext())
                     .load(photoUri)
-                    .error(R.drawable.ic_add) // Imagen predeterminada si no se puede cargar la imagen
+                    .error(R.drawable.ic_add) // // Show a default image if the photoUri cannot be loaded
                     .into(holder.recipePhotoView);
         } else {
-            // Si no hay imagen, muestra una imagen predeterminada
+            // If there is no image, show a default image
             Glide.with(holder.itemView.getContext())
                     .load(R.drawable.ic_add)
                     .into(holder.recipePhotoView);
         }
+
+        // Set the click listener for the item view
         holder.itemView.setOnClickListener(view -> {
             if (recipeClickListener != null) {
+                // Invoke the callback method when the item view is clicked
                 recipeClickListener.onRecipeClick(title, photoUri, detail);
             }
         });
     }
 
+    /**
+     * Returns the total number of recipes in the data set.
+     *
+     * @return The total number of recipes in the data set.
+     */
     @Override
     public int getItemCount() {
         return titles.size();
     }
 
+    /**
+     * Interface definition for a callback to be invoked when a recipe item is clicked in the RecyclerView.
+     */
     public interface OnRecipeClickListener {
+
+        /**
+         * Called when a recipe item is clicked.
+         *
+         * @param title   The title of the clicked recipe.
+         * @param photo   The Uri of the photo associated with the clicked recipe, or null if no photo is available.
+         * @param content The content/details of the clicked recipe.
+         */
         void onRecipeClick(String title, Uri photo, String content);
     }
 
+    /**
+     * Sets the listener for handling recipe item click events.
+     *
+     * @param listener The listener to be set for handling recipe item click events.
+     */
     public void setOnRecipeClickListener(OnRecipeClickListener listener) {
         this.recipeClickListener = listener;
     }
 
+    /**
+     * ViewHolder for holding the views of a recipe item in the RecyclerView.
+     */
     static class RecipeHolder extends RecyclerView.ViewHolder {
+
+        /**
+         * TextView and ImageView to display the title, resume,
+         * photo and details of the recipe.
+         */
         public final TextView recipeTitleView;
         public final TextView recipeResumeView;
         public final ImageView recipePhotoView;
@@ -92,6 +153,11 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeHold
         public final TextView recipeDetailContentView;
         public final ImageView recipeDetailPhotoView;
 
+        /**
+         * Constructor for the RecipeHolder.
+         *
+         * @param recipeView The View that contains the views for a recipe item.
+         */
         public RecipeHolder(@NonNull View recipeView) {
             super(recipeView);
             LinearLayout layout = recipeView.findViewById(R.id.recipe_layout);
